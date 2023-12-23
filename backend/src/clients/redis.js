27a8a20a46ec21123259602e5685eb8e0f72b82b
Ritesh Kumar <singlaritesh007@gmail.com>
2023-12-23 15:47:25 +0530

    env setup

--- backend/src/clients/redis.js
+++ backend/src/clients/redis.js
@@ -1,8 +1,8 @@
 const Redis = require("ioredis")
 const redis = new Redis({
-  port: 19380, // Redis port
-  host: "redis-19380.c301.ap-south-1-1.ec2.cloud.redislabs.com", // Redis host
-  password: "Sv6DvDWyShs38EpZzyaBf07WRKAkSUsF",
+  port: process.env.REDIS_PORT, // Redis port
+  host: process.env.REDIS_HOST, // Redis host
+  password: process.env.REDIS_PASSWORD,
 })
 
 export default redis
