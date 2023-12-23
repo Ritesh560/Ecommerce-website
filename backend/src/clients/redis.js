7f3df84b10b366ffc566f51528b7e1afe4a21ca4
Ritesh Kumar <singlaritesh007@gmail.com>
2023-12-23 15:39:31 +0530

    feat: setting up for deploy

--- backend/src/clients/redis.js
+++ backend/src/clients/redis.js
@@ -1,4 +1,8 @@
 const Redis = require("ioredis")
-const redis = new Redis()
+const redis = new Redis({
+  port: 19380, // Redis port
+  host: "redis-19380.c301.ap-south-1-1.ec2.cloud.redislabs.com", // Redis host
+  password: "Sv6DvDWyShs38EpZzyaBf07WRKAkSUsF",
+})
 
 export default redis
